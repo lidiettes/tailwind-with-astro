a795845d85de3a6e54157dda39d24b9c06b203ad
lidiettes <hello@lidiettes.com>
2023-01-16 22:18:50 +0100

    configuration repo

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <div>Home de Astro</div>
+    <p class= "font-bold">una pruebecita </p>
+    </>
+  )
+}
+
+export default Home
